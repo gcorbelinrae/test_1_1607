1104046a2e8d5c2bda6e80ff1228888d341c38d9
gcorbelinrae <gaelle.corbel@inrae.fr>
2024-07-16 16:13:39 +0200

    Implémentation des armes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Personnage import Personnage
+from Arme import Arme
 
 # Exemple d'utilisation de la classe Personnage
-
-joueur1 = Personnage("Aragorn", "Guerrier", 10, 100, 15, 5)
+epee_legendaire = Arme("Epée légendaire", 10)
+joueur1 = Personnage("Aragorn", "Guerrier", 10, 100, 15, 5, epee_legendaire)
 joueur2 = Personnage("Gandalf", "Magicien", 10, 80, 5, 15)
 
 joueur1.afficher_info()
